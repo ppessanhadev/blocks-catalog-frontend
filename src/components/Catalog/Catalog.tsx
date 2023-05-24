cecbba7786b8ea0b159c1bd41d663c53678a77a0
Pablo Pessanha <pablopessanh@gmail.com>
2023-05-24 01:59:40 -0300

    feat: adds reponsive catalog

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -7,7 +7,7 @@ export const Catalog = () => {
     <section className="px-16 lg:px-48 m-auto py-8">
       <h3 className="text-blocks-black text-2xl font-medium p-1">Resultados</h3>
 
-      <main className="flex flex-wrap gap-4">
+      <main className="flex flex-wrap justify-center lg:justify-start gap-4">
         {arr.map(({ id }) => (
           <FamilyCard key={id} />
         ))}
