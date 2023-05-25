9990a31431c837ec7042b15efd074f764c0af495
Pablo Pessanha <pablopessanh@gmail.com>
2023-05-25 02:35:19 -0300

    fix: adds error treat when image is broken

--- src/components/Catalog/FamilyCard.tsx
+++ src/components/Catalog/FamilyCard.tsx
@@ -1,29 +1,36 @@
 import Image from 'next/image';
 import { ScriptProps } from 'next/script';
 import { Family } from '@/types/Family';
+import { useState } from 'react';
 
 interface FamilyCardProps extends ScriptProps {
   family: Family;
 }
 
-export const FamilyCard = ({ family }: FamilyCardProps) => (
-  <article className="relative w-56 h-72 md:w-48 md:h-64 border border-blocks-gray rounded-xl">
-    <Image
-      width={130}
-      height={130}
-      priority={true}
-      src={`https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/${family?.id}.jpg`}
-      alt="Family image"
-      className="m-auto w-auto h-auto p-2"
-    />
-    <div className="flex justify-around absolute bottom-0 w-full rounded-b-xl border-t border-t-blocks-gray p-1">
-      <p className="inline text-blocks-black text-xs font-semibold w-36 leading-3">{family?.details?.name}</p>
+export const FamilyCard = ({ family }: FamilyCardProps) => {
+  const [error, setError] = useState(false);
+  const imageSrc = `https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/${family?.id}.jpg`;
 
-      <span className="bg-blocks-gray w-[2px] h-6 mx-2" />
+  return (
+    <article className="relative w-56 h-72 md:w-48 md:h-64 border border-blocks-gray rounded-xl">
+      <Image
+        width={180}
+        height={180}
+        priority={true}
+        src={error ? '/no-image.jpg' : imageSrc}
+        alt="Family image"
+        className="m-auto w-auto h-auto p-2"
+        onError={() => setError(true)}
+      />
+      <div className="flex justify-around absolute bottom-0 w-full rounded-b-xl border-t border-t-blocks-gray p-1">
+        <p className="inline text-blocks-black text-xs font-semibold w-36 leading-3">{family?.details?.name}</p>
 
-      <button>
-        <Image src="/arrow-up-right.svg" width={16} height={16} alt="Seta para direita" />
-      </button>
-    </div>
-  </article>
-);
+        <span className="bg-blocks-gray w-[2px] h-6 mx-2" />
+
+        <button>
+          <Image src="/arrow-up-right.svg" width={16} height={16} alt="Seta para direita" />
+        </button>
+      </div>
+    </article>
+  );
+};
