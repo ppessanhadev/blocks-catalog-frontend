facc6a0b5205e65c8619679fc539882f043ab631
Pablo Pessanha <pablopessanh@gmail.com>
2023-05-25 03:36:27 -0300

    chore: extends timeout limit

--- src/services/GetFamilies.ts
+++ src/services/GetFamilies.ts
@@ -5,7 +5,7 @@ import { config } from '@/utils/config';
 export async function getFamilies(skip = 0) {
   const { data: families } = await axios.get<Family[]>(config.baseUrl.api, {
     params: { take: 50, skip },
-    timeout: 30000,
+    timeout: 60000,
   });
 
   return families;
